6c7839604e5c8e122736a23f37a2696adbfacfa0
garamond13 <98652255+garamond13@users.noreply.github.com>
2022-09-28 03:42:08 +0200

    switch to !HOOK OUTPUT
    it gives better quality

--- unsharp_masking.glsl
+++ unsharp_masking.glsl
@@ -1,11 +1,12 @@
 //!DESC unsharp masking
-//!HOOK SCALED
+//!HOOK OUTPUT
 //!BIND HOOKED
 //!WHEN NATIVE_CROPPED.h OUTPUT.h <
 
-#define param 0.5 //sharpening strenght
+#define PARAM 0.5 //sharpening strenght
 
-vec4 hook(){
+vec4 hook()
+{
     float st1 = 1.2;
     vec4 p = HOOKED_tex(HOOKED_pos);
     vec4 sum1 = HOOKED_texOff(st1 * vec2(+1, +1))
@@ -18,5 +19,5 @@ vec4 hook(){
               + HOOKED_texOff(st2 * vec2(-1,  0))
               + HOOKED_texOff(st2 * vec2( 0, -1));
     vec4 t = p * 0.859375 + sum2 * -0.1171875 + sum1 * -0.09765625;
-    return p + t * param;
+    return p + t * PARAM;
 }
